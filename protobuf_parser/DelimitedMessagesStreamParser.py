40a74fda0a4cc9b4d056c4ac9f588099e49d52e3
ValikSilchenko <valik.silchenko@gmail.com>
2023-03-20 13:42:14 +0300

    fix: added message_type to parser class, added TypeError catching

--- protobuf_parser/DelimitedMessagesStreamParser.py
+++ protobuf_parser/DelimitedMessagesStreamParser.py
@@ -1,22 +1,25 @@
 from protobuf_parser.helpers import parse_delimited
-import protobuf_parser.pr_pb2 as pb
 
 
 class DelimitedMessagesStreamParser:
-    __slots__ = "_buffer"
+    __slots__ = ["_buffer", "_message_type"]
 
-    def __init__(self):
+    def __init__(self, message_type: type):
         self._buffer = bytes()
+        self._message_type = message_type
 
-    def parse(self, data: bytes) -> list[pb.WrapperMessage]:
+    def parse(self, data: bytes) -> list:
         parsed_msgs = []
-        self._buffer += data
+        try:
+            self._buffer += data
+        except TypeError:
+            return []
 
         bytes_consumed = -1
         while bytes_consumed != 0:
             message, bytes_consumed = parse_delimited(self._buffer)
-            if message is not None:
+            if message:
                 parsed_msgs.append(message)
-                self._buffer = self._buffer.removeprefix(self._buffer[:bytes_consumed])
+                self._buffer = self._buffer[bytes_consumed:]
 
         return parsed_msgs
